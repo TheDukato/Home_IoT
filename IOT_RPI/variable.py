aa4de5246845d64d60c4e3d4cacac77cb2885861
TheDukato <kpokusa99@gmail.com>
2024-11-17 11:37:34 +0100

    Update RPI code to current version

---
+++ IOT_RPI/variable.py
@@ -0,0 +1,20 @@
+counters = [
+    {
+        "name": "L1",
+        "counter_file": "/home/kamil/IOT_RPI/node-red/counter_1.txt",
+        "sensor_file": "/home/kamil/IOT_RPI/node-red/sensor_1.txt",
+        "meter_constant": 2500
+    },
+    {
+        "name": "L2",
+        "counter_file": "/home/kamil/IOT_RPI/node-red/counter_2.txt",
+        "sensor_file": "/home/kamil/IOT_RPI/node-red/sensor_2.txt",
+        "meter_constant": 1000
+    },
+    {
+        "name": "L3",
+        "counter_file": "/home/kamil/IOT_RPI/iNode/counter_3.txt",
+        "sensor_file": "/home/kamil/IOT_RPI/iNode/sensor_3.txt",
+        "meter_constant": 10000
+    }
+]
